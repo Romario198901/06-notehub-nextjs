307cb6b911b25fa2b4280deaf8d9d7d8d579efd8
Roman Hrydin <romario198901@gmail.com>
2026-02-18 21:39:44 +0200

    add formattimg

--- app/notes/[id]/error.tsx
+++ app/notes/[id]/error.tsx
@@ -1,9 +1,7 @@
-'use client'
+"use client";
 interface ErrorProps {
   error: Error;
-};
-export default function Error({error}:ErrorProps) {
-    return (
-        <p>Could not fetch note details.{ error.message}</p>
-    )
+}
+export default function Error({ error }: ErrorProps) {
+  return <p>Could not fetch note details.{error.message}</p>;
 }
